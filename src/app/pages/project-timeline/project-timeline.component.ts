d8762039b8d72221fddb61eddec807ac8a41c551
NickolayKiritsa <129768892+NickolayKiritsa@users.noreply.github.com>
2025-06-05 17:03:08 +0300

    update header

--- src/app/pages/project-timeline/project-timeline.component.ts
+++ src/app/pages/project-timeline/project-timeline.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {AboutUsComponent} from '../about-us/about-us.component';
 import {TitleComponent} from '../../layouts/title/title.component';
 import {TranslocoPipe} from '@jsverse/transloco';
 
