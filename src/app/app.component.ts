03290df785a1aa16da498912c7f8519a165bdbd5
NickolayKiritsa <129768892+NickolayKiritsa@users.noreply.github.com>
2025-06-06 16:42:09 +0300

    update form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {HeaderComponent} from './layouts/header/header.component';
 import {FooterComponent} from './layouts/footer/footer.component';
 import {RouterOutlet} from '@angular/router';
 import {NgxScrollTopComponent} from 'ngx-scrolltop';
+import {GeoService} from './core/services/geo.service';
 
 
 @Component({
@@ -19,5 +20,10 @@ import {NgxScrollTopComponent} from 'ngx-scrolltop';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private geoService= inject(GeoService);
+
+  constructor() {
+    this.geoService.getCountry()
+  }
 
 }
