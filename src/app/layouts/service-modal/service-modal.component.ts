dd95cbda4328bc9cd1c65219690a94909701dcb6
NickolayKiritsa <129768892+NickolayKiritsa@users.noreply.github.com>
2025-06-06 12:50:01 +0300

    update services

---
+++ src/app/layouts/service-modal/service-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-modal',
+  imports: [],
+  templateUrl: './service-modal.component.html',
+  styleUrl: './service-modal.component.scss'
+})
+export class ServiceModalComponent {
+
+}
