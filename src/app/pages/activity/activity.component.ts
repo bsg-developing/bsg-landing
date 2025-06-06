584babc0d3d25881cf6a92c4612d01e473359fbb
NickolayKiritsa <129768892+NickolayKiritsa@users.noreply.github.com>
2025-06-06 12:56:31 +0300

    update services

--- src/app/pages/activity/activity.component.ts
+++ src/app/pages/activity/activity.component.ts
@@ -3,6 +3,8 @@ import {AboutUsComponent} from '../about-us/about-us.component';
 import {TitleComponent} from '../../layouts/title/title.component';
 import {SERVICES} from '../../core/constants/services';
 import {NgForOf} from '@angular/common';
+import {MatDialog} from '@angular/material/dialog';
+import {ServiceModalComponent} from '../../layouts/service-modal/service-modal.component';
 
 @Component({
   selector: 'app-activity',
@@ -16,4 +18,16 @@ import {NgForOf} from '@angular/common';
 })
 export class ActivityComponent {
   services = SERVICES;
+
+  constructor(private dialog: MatDialog) {}
+
+  openModal(service: { title: string; description: string }) {
+    this.dialog.open(ServiceModalComponent, {
+      data: {
+        title: service.title,
+        description: service.description
+      },
+      width: '400px'
+    });
+  }
 }
