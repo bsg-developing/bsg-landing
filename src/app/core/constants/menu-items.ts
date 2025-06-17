f9c2bd6320bd213ab03dea75f67c52a0aa9286fa
NickolayKiritsa <129768892+NickolayKiritsa@users.noreply.github.com>
2025-06-17 17:27:00 +0300

    added link for trusted companies

--- src/app/core/constants/menu-items.ts
+++ src/app/core/constants/menu-items.ts
@@ -7,5 +7,6 @@ export const MENU_ITEMS: MenuItem[] = [
   { fragment: 'activity', labelKey: 'aboutUs' },
   { fragment: 'Approach', labelKey: 'Approach' },
   { fragment: 'technologies', labelKey: 'technologies' },
+  { fragment: 'trust', labelKey: 'trust' },
   { fragment: 'contact-us', labelKey: 'contactUs' }
 ];
