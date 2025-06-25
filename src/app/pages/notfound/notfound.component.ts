988b71f045dbe2a25bec0c8a990159f7c372ccf0
NickolayKiritsa <129768892+NickolayKiritsa@users.noreply.github.com>
2025-06-25 11:09:27 +0300

    update notfound component

--- src/app/pages/notfound/notfound.component.ts
+++ src/app/pages/notfound/notfound.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {TranslocoPipe} from '@jsverse/transloco';
 
 @Component({
   selector: 'app-notfound',
-  imports: [],
+  imports: [
+    TranslocoPipe
+  ],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.scss'
 })
 export class NotfoundComponent {
+    private router = inject(Router);
 
+    goHome(): void {
+      this.router.navigate(['/']);
+    }
 }
