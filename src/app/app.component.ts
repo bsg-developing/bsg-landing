a0147f8e00392bb106e83df530c0e6a5bb7137b4
nikol <@nkiritsa>
2025-07-18 19:47:26 +0300

    update seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import {Component, inject} from '@angular/core';
 import {HeaderComponent} from './layouts/header/header.component';
 import {FooterComponent} from './layouts/footer/footer.component';
-import {ActivatedRoute, NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {RouterOutlet} from '@angular/router';
 import {NgxScrollTopComponent} from 'ngx-scrolltop';
 import {ViewportScroller} from '@angular/common';
-import { TranslocoService } from '@jsverse/transloco';
-import {distinctUntilChanged, filter, map} from 'rxjs';
-import {Meta, Title } from '@angular/platform-browser';
+import {SeoService} from './core/services/seo.service';
 
 
 @Component({
@@ -23,50 +21,11 @@ import {Meta, Title } from '@angular/platform-browser';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
   private vs = inject(ViewportScroller);
+  private seoService = inject(SeoService);
 
-
-
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private translocoService: TranslocoService,
-    private titleService: Title,
-    private metaService: Meta
-  ) {
-    this.router.events
-      .pipe(
-        filter(event => event instanceof NavigationEnd),
-        map(() => this.route.firstChild?.snapshot.paramMap.get('lang')),
-        distinctUntilChanged()
-      )
-      .subscribe(lang => {
-        if (lang && ['ru', 'en', 'ro'].includes(lang)) {
-          this.translocoService.setActiveLang(lang);
-          const meta = META_INFO[lang];
-          if (meta) {
-            this.titleService.setTitle(meta.title);
-            this.metaService.updateTag({ name: 'description', content: meta.description });
-          }
-        }
-
-      });
+  constructor() {
+    this.seoService.initializeMetaTags();
     this.vs.setOffset([0, 100]);
   }
-
 }
-const META_INFO: Record<string, { title: string; description: string }> = {
-  ru: {
-    title: 'Solterprise | Мы создаём цифровой бизнес',
-    description: 'Solterprise — команда, которой доверяют. Создание сайтов, SEO, поддержка. Работаем по Молдове и за её пределами. ☎ +373 79 294 106."'
-  },
-  en: {
-    title: 'Solterprise | We Build Digital Business',
-    description: 'Solterprise — a team you can trust. Website development, SEO, support. We work across Moldova and beyond. ☎ +373 79 294 106.'
-  },
-  ro: {
-    title: 'Solterprise | Construim afaceri digitale',
-    description: 'Solterprise — o echipă de încredere. Creare site-uri, SEO, suport. Activăm în toată Moldova și în afara ei. ☎ +373 79 294 106.'
-  }
-};
---
+++ src/app/core/services/seo.service.ts
@@ -0,0 +1,54 @@
+import {inject, Injectable } from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {TranslocoService} from '@jsverse/transloco';
+import {Meta, Title} from '@angular/platform-browser';
+import {distinctUntilChanged, filter, map} from 'rxjs';
+import {DOCUMENT} from '@angular/common';
+import {META_INFO} from '../constants/meta.constant';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeoService {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private translateService = inject(TranslocoService);
+  private titleService = inject(Title);
+  private metaService = inject(Meta);
+  private document = inject(DOCUMENT);
+
+  public initializeMetaTags() {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.route.firstChild?.snapshot.paramMap.get('lang')),
+        distinctUntilChanged()
+      )
+      .subscribe(lang => {
+        const currentLang = (lang && ['ru', 'en', 'ro'].includes(lang)) ? lang : 'en';
+        this.translateService.setActiveLang(currentLang);
+
+        const meta = META_INFO[currentLang];
+        if (meta) {
+          this.updateMetaTags(currentLang, meta.title, meta.description, meta.keywords);
+        }
+      });
+  }
+
+  private updateMetaTags(lang: string, title: string, description: string, keywords: string) {
+    this.titleService.setTitle(title);
+
+    this.metaService.updateTag({ name: 'description', content: description });
+    this.metaService.updateTag({ name: 'keywords', content: keywords });
+
+    this.metaService.updateTag({ property: 'og:title', content: title });
+    this.metaService.updateTag({ property: 'og:description', content: description });
+
+    this.metaService.updateTag({ name: 'twitter:title', content: title });
+    this.metaService.updateTag({ name: 'twitter:description', content: description });
+
+    if (this.document?.documentElement) {
+      this.document.documentElement.lang = lang;
+    }
+  }
+}
